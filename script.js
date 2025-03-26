1fb4d3fa757f5a4e3d2c6cb602392edade4f9612
Jhostin0_0 <jhostin.qh@gmail.com>
2025-03-26 17:01:11 -0500

    a

--- script.js
+++ script.js
@@ -1,45 +1,15 @@
-document.getElementById('inscripcionForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.getElementById("inscripcionForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita el envío del formulario
 
-    const nombre = document.getElementById('nombre').value;
-    const edad = parseInt(document.getElementById('edad').value);
-    const experiencia = document.getElementById('experiencia').value;
+    let objetivo = document.getElementById("objetivo").value;
 
-    const mensajeDiv = document.getElementById('mensaje');
-    mensajeDiv.style.display = 'block';
-
-    if (edad < 12 || edad > 100) {
-        mensajeDiv.className = 'mensaje error';
-        mensajeDiv.textContent = 'Error: La edad debe estar entre 12 y 100 años.';
-        return;
-    }
-
-    if (!nombre || !edad || !experiencia) {
-        mensajeDiv.className = 'mensaje error';
-        mensajeDiv.textContent = 'Por favor, complete todos los campos.';
-        return;
-    }
-
-    let mensaje = '';
-
-    if (edad > 18 && edad <= 35) {
-        switch (experiencia) {
-            case 'Principiante':
-                mensaje = 'Buen Momento para empezar';
-                break;
-            case 'Intermedio':
-                mensaje = 'Vamos Avanzando';
-                break;
-            case 'Avanzado':
-                mensaje = 'Prepárate para Liderar';
-                break;
-        }
-    } else if (edad > 35) {
-        mensaje = 'Nunca es tarde para aprender.';
+    if (objetivo === "perder_peso") {
+        window.location.href = "PerderPeso.html";
+    } else if (objetivo === "mantenerme") {
+        window.location.href = "Mantenerme.html";
+    } else if (objetivo === "ganar_musculo") {
+        window.location.href = "GanarMusculo.html";
     } else {
-        mensaje = 'Genial joven programador, te recomendamos cursos básicos para iniciar.';
+        alert("Por favor, selecciona un objetivo válido.");
     }
-
-    mensajeDiv.className = 'mensaje success';
-    mensajeDiv.textContent = mensaje;
 });
